9f97a41e340811cf7b539e94da842b29b285a961
Owtter <owen.mccloe@gmail.com>
2022-01-26 22:02:16 +0100

    V1.1.1 - HTML/CSS: Edited slide-out menu

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -1,3 +1,17 @@
 console.log("oi! - app.js")
 
-// pull-out menu
+// slide-out nav
+function openNav() {
+    document.getElementById('mySidenav').style.width="250px";
+    document.getElementById('closeBtn').style.display="initial";
+}
+
+function closeNav() {
+    document.getElementById('mySidenav').style.width="0";
+    document.getElementById('closeBtn').style.display="none";
+}
+
+// drop down MyFiles
+function openMyFilesDropdown() {
+    document.getElementById("myFilesDropdownContent").classList.toggle("show");
+}
