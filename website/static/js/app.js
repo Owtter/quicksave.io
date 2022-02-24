ed55832cb38cea9e430a4c9bd28d215ad2a2e72a
Owtter <owen.mccloe@gmail.com>
2022-02-24 06:49:09 +0100

    V1.1.1 - HTML/CSS/JS: Edited Complete web template redesign

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -1,17 +1,6 @@
 console.log("oi! - app.js")
 
+// dynamic year in footer
+document.getElementById("copyright-year").innerHTML = (new Date().getFullYear());
+
 // slide-out nav
-function openNav() {
-    document.getElementById('mySidenav').style.width="250px";
-    document.getElementById('closeBtn').style.display="initial";
-}
-
-function closeNav() {
-    document.getElementById('mySidenav').style.width="0";
-    document.getElementById('closeBtn').style.display="none";
-}
-
-// drop down MyFiles
-function openMyFilesDropdown() {
-    document.getElementById("myFilesDropdownContent").classList.toggle("show");
-}
